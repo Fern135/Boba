021d7b352a721a56771b09f57da262cc55bbf956
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-07-31 18:26:17 -0400

    made the begining of the run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    print("hello from the main file")
+    
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except Exception as e:
+        print(f"error: {str(e)}")
