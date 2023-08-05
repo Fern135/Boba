e768a8fd2cf3974246f99ba7e64c4e5a1ff186e2
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-08-05 16:09:39 -0400

    i don't know

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import argparse
-import sys
+import asyncio
+import time
+# import sys
 
-from lib.util.util           import create_and_write_to_file
+from lib.util.util           import create_and_write_to_file, read_json_file, get_absolute_path, run_terminal_command
 from lib.datetime.dt         import get_current_date_with_full_month, get_current_time_12
 
 from lib.multiprocess.worker import Worker # background worker
@@ -20,29 +22,58 @@ from lib.multiprocess.worker import Worker # background worker
 
 from src.php_server.php_svr  import PHP    
 from src.dns.dns             import DNSServer
-
 from src.tests.test_all      import *
 
+is_dev = read_json_file(get_absolute_path("./bin/conf/conf.json"), "development")
 
-def cli():
+def git_commands(commit_message):    
+    run_terminal_command("git status")
+
+    user_input = str(input("add all (y) or specific 1 manually(n)\n"))
+
+    if user_input.lower() == "y":
+        run_terminal_command('git add .')
+        # time.sleep(500)
+        run_terminal_command(f'git commit -m {commit_message}')
+        # time.sleep(500)
+        run_terminal_command("git status")
+        time.sleep(5)
+        run_terminal_command("git push origin")
+
+    else:
+        return
+
+
+
+async def cli():
     try:
         parser = argparse.ArgumentParser(description="******************** Buba Cli ********************")
-        parser.add_argument("--test", help="run the tests using pytest", default="all")
-        parser.add_argument("--run", help="run development server", default="dev")
-        parser.add_argument("--gitcommit", help="run development server", dest="gitcommit", type=str, default="gitcmmt")
 
-        args = parser.parse_args(sys.argv[1:])
+        parser.add_argument("--test", action="store_true", help="Run all tests.")
+        parser.add_argument("--run", action="store_true", help="running development or production server")
+        parser.add_argument("--git", help="will run some git commands", type=str)
 
-        run_server, gitcommit = args.run, args.gitcommit
+        args = parser.parse_args()
 
         if args.test:
             test_all()
 
-        if args.run:
-            print(f"dev run server {run_server}")
+        elif args.run:
+            if is_dev is True:
+                print("running development server")
 
-        if args.gitcommit:
-            print(f"dev {gitcommit}")
+            else:
+                print("running normal server")
+
+        elif args.git:
+            git_message = args.git
+
+            git_commands(git_message)
+        else:
+            print("No args passed")
+
+
+        return 
 
     except Exception as e:
         print(f"error cli: {str(e)}")
@@ -54,8 +85,9 @@ def cli():
 
 
 def main():
-    cli()
-    
+    result = asyncio.run(cli())
+    print(result)
+
 
 
 if __name__ == "__main__":
