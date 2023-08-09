818ac091bf5fef8f85f7991c867255ae477e1d8f
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2023-08-08 20:57:02 -0400

    modified:   src/panel/frontend/panel/package-lock.json 	modified:   src/panel/frontend/panel/package.json 	modified:   src/panel/frontend/panel/src/components/php_server/component/php_server.js

--- src/panel/frontend/panel/src/components/php_server/component/php_server.js
+++ src/panel/frontend/panel/src/components/php_server/component/php_server.js
@@ -1,20 +1,48 @@
-import React, { 
-    // useEffect,
-    useState 
-} from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/php_server.scss';
 
 function PhpServer() {
-    const [isRunning, setIsRunning] = useState(true);
+    const [isRunning,              setIsRunning] = useState(true);
+    const [projects,                setProjects] = useState([]);
+    const [selectedProject,  setSelectedProject] = useState('');
+    const [projectDetails,    setProjectDetails] = useState({});
 
     const toggleRunning = () => {
         setIsRunning(!isRunning);
     };
 
+    const fetchProjects = async () => {
+        try {
+            const response = await axios.get('/api/projects'); // Replace with your server API endpoint
+            setProjects(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchProjectDetails = async (projectId) => {
+        try {
+            const response = await axios.get(`/api/project/${projectId}`); // Replace with your server API endpoint
+            setProjectDetails(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    const handleProjectChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedProject(selectedValue);
+        fetchProjectDetails(selectedValue);
+    };
+
     const PhpLogs = () => {
-        return(
+        return (
             <div className='border PhpLogs'>
                 <p className='green'>
                     {isRunning}
@@ -24,16 +52,10 @@ function PhpServer() {
         );
     };
 
-    // useEffect(() => {
-    //     PhpLogs();
-    // });
-
     return (
         <div className="container border">
-            <h1>
-                Php Server
-            </h1>
-            <hr></hr>
+            <h1>Php Server</h1>
+            <hr />
 
             <div className='row'>
                 <div className="col col-sm col-md">
@@ -41,33 +63,47 @@ function PhpServer() {
                         className={`btn btn-block ${isRunning ? 'btn-success' : 'btn-danger'}`}
                         onClick={toggleRunning}
                     >
-                    <h3>{isRunning ? 'Start Server' : 'Stop Server'}</h3>
+                        <h3>{isRunning ? 'Start Server' : 'Stop Server'}</h3>
                     </button>
 
-                    <hr></hr>
-                    
+                    <hr />
+
                     <div className="status-container">
                         <div className={`circle ${isRunning ? 'red' : 'green'}`} />
                         <h3>Status</h3>
                     </div>
                 </div>
 
-                <div className="col col-sm col-md border">
-                    <h3>
-                        {/* show drop down of registered projects / domains in http_serve */}
-                        project: {}
-                    </h3>
-                    <h4>
-                        name: {}
-                        <br></br>
-                        path: {}
-                    </h4>
-                </div>
-
                 <div className="col col-sm col-md">
                     <PhpLogs />
                 </div>
 
+                <div className='row'>
+                    <div className="col col-sm col-md border">
+                        <h3>Project:</h3>
+                        <select value={selectedProject} onChange={handleProjectChange}>
+                            <option value="">Select a project</option>
+
+                            {projects.map((project, index) => (
+                                <option key={index} value={project.id}>
+                                    {project.name}
+                                </option>
+                            ))}
+
+                        </select>
+                        {selectedProject && (
+                            <div>
+                                <h4>
+                                    Domain name: {projectDetails.domainName}
+                                    <br />
+                                    Project path: {projectDetails.projectPath}
+                                    <br />
+                                    PHP version: {projectDetails.phpVersion}
+                                </h4>
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
