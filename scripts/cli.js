48afebb22b6f042b0daacf89c11b9493c44a83c3
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-05-01 15:56:55 -0400

    tried to debug some custom scripts. still debugging.

--- scripts/cli.js
+++ scripts/cli.js
@@ -8,55 +8,63 @@ const rl = readline.createInterface({
 });
 
 function setUpEnv(){
-  let command;
-  switch (process.platform) {
-  case 'win32':
+  try{
+    let command;
+    switch (process.platform) {
+      case 'win32':
         command = 'python ./setup/env.py';
-    break;
-    
+        break;
+        
     case 'darwin':
       case 'linux':
         command = 'python3 ./setup/env.py';
-    break;
-  default:
-    console.error(`Unsupported platform: ${process.platform}`);
-    process.exit(1);
-  }
-  
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+        break;
+    default:
+      console.error(`Unsupported platform: ${process.platform}`);
+      process.exit(1);
     }
-    console.log(stdout);
-    if (stderr) console.error(`stderr: ${stderr}`);
-  });
+            
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(stdout);
+      if (stderr) console.error(`stderr: ${stderr}`);
+    });
+  }catch(error){
+    console.log(`error in setting up env\nerror: ${error.toString()}`);
+  }
 }
 
 function deploy(){
-  let command;
-  switch (process.platform) {
-  case 'win32':
-        command = 'python ./deploy/deploy.py';
-    break;
-    
-    case 'darwin':
-      case 'linux':
-        command = 'python3 ./deploy/deploy.py';
-    break;
-  default:
-    console.error(`Unsupported platform: ${process.platform}`);
-    process.exit(1);
-  }
-
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+  try{
+    let command;
+    switch (process.platform) {
+    case 'win32':
+          command = 'python ./deploy/deploy.py';
+      break;
+      
+      case 'darwin':
+        case 'linux':
+          command = 'python3 ./deploy/deploy.py';
+      break;
+    default:
+      console.error(`Unsupported platform: ${process.platform}`);
+      process.exit(1);
     }
-    console.log(stdout);
-    if (stderr) console.error(`stderr: ${stderr}`);
-  });
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(stdout);
+      if (stderr) console.error(`stderr: ${stderr}`);
+    });
+  }catch(error){
+    console.log(`error in deploying app\nerror: ${error.toString()}`);
+  }
 }
 
 const args = process.argv.slice(2);
--- scripts/setup/env.py
+++ scripts/setup/env.py
@@ -4,24 +4,33 @@ import string
 CHARACTERS = string.ascii_uppercase + string.digits + \
     string.ascii_lowercase + string.punctuation + string.ascii_letters
 
-def generate_random_alphanumeric_string(length: int) -> str:
+def generate_random_alphanumeric_string(length: int):
     return ''.join(random.choice(CHARACTERS) for _ in range(length))
 
 def write_env_file():
     # todo: debug why this keeps running in the terminal. 
-    env_data = {
-        "secret_key": generate_random_alphanumeric_string(random.randint(1, 250)) + "\n",
-        "db_user_name": "",
-        "db_password": "",
-        "db_ip": "",
-        "db_name": "",
-        "debugging": "True"
-    }
+    # env_data = {
+    #     "secret_key": generate_random_alphanumeric_string(random.randint(1, 250)) + "\n",
+    #     "db_user_name": "",
+    #     "db_password": "",
+    #     "db_ip": "",
+    #     "db_name": "",
+    #     "debugging": "True"
+    # }
+
+    env_data = f"""
+        secret_key={generate_random_alphanumeric_string(255)}\n\n
+        db_user_name=\n
+        db_password=\n
+        db_ip=localhost\n
+        db_name=Boba\n
+        debugging=True\n
+    """
 
     file_path = "../../.env"
     with open(file_path, "w") as env_file:
-        for key, value in env_data.items():
-            env_file.write(f"{key}={value}\n")
+        # for key, value in env_data.items():
+        env_file.write(env_data)
 
     print(f".env file written successfully at {file_path}")
 
