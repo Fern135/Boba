5f34933d249788fede2d73748cd9da8ac6e30891
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2024-05-01 12:56:54 -0400

    refactored some unneeded comments

--- scripts/setup/env.py
+++ scripts/setup/env.py
@@ -5,19 +5,6 @@ CHARACTERS = string.ascii_uppercase + string.digits + \
     string.ascii_lowercase + string.punctuation + string.ascii_letters
 
 def generate_random_alphanumeric_string(length: int) -> str:
-    """
-        Generate a random alphanumeric string of a specified length.
-
-        Parameters:
-            length (int): The length of the random alphanumeric string to be generated.
-
-        Returns:
-            str: A random alphanumeric string of the specified length.
-
-        Example:
-            random_string = generate_random_alphanumeric_string(10)
-            print(random_string)  # Output: "AbCdEfGhIj"
-    """
     return ''.join(random.choice(CHARACTERS) for _ in range(length))
 
 def write_env_file():
