4513a88a92e1633cf6c9553442315a10f5ec2982
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2024-05-01 12:53:06 -0400

    added cli.go to replace cli.js
    reverted env.py to old since it just works.

--- scripts/setup/env.py
+++ scripts/setup/env.py
@@ -4,13 +4,29 @@ import string
 CHARACTERS = string.ascii_uppercase + string.digits + \
     string.ascii_lowercase + string.punctuation + string.ascii_letters
 
-if __name__ == "__main__":
+def generate_random_alphanumeric_string(length: int) -> str:
+    """
+        Generate a random alphanumeric string of a specified length.
+
+        Parameters:
+            length (int): The length of the random alphanumeric string to be generated.
+
+        Returns:
+            str: A random alphanumeric string of the specified length.
+
+        Example:
+            random_string = generate_random_alphanumeric_string(10)
+            print(random_string)  # Output: "AbCdEfGhIj"
+    """
+    return ''.join(random.choice(CHARACTERS) for _ in range(length))
+
+def write_env_file():
     env_data = {
-        "secret_key": ''.join(random.choice(CHARACTERS) for _ in range(random.randint(1, 250))) + "\n",
+        "secret_key": generate_random_alphanumeric_string(random.randint(1, 250)) + "\n",
         "db_user_name": "",
         "db_password": "",
         "db_ip": "",
-        "db_name": ""+ "\n",
+        "db_name": "",
         "debugging": "True"
     }
 
@@ -21,4 +37,5 @@ if __name__ == "__main__":
 
     print(f".env file written successfully at {file_path}")
 
-    
+if __name__ == "__main__":
+    write_env_file()
