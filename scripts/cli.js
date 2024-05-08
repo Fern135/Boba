9b5365a9e64d0df062d4d564c216911284a04dc3
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-05-08 19:25:11 -0400

    worked on adding a update language and database version to be up to date.
    and deleted un needed code on util.go

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,6 +1,12 @@
 const readline = require('readline');
 const { exec } = require('child_process');
 
+const help = `
+  commands\n\t
+    -env:\tWrite env variable data to .env file in directory.\n
+    -d:\tDeploy application on the landing page (not working yet.)\n
+    -upgrade:\tUpgrade conf.json with latest go, php, python, node and npm version\n
+`;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -65,10 +71,46 @@ function deploy(){
   }
 }
 
+function update(){
+  try{
+    let command;
+    switch (process.platform) {
+      case 'win32':
+        command = 'python ./update/update.py';
+        break;
+    case 'darwin':
+    case 'linux':
+        command = 'python3 ./update/update.py';
+        break;
+    default:
+      console.error(`Unsupported platform: ${process.platform}`);
+      process.exit(1);
+    }
+            
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(stdout);
+      if (stderr) console.error(`stderr: ${stderr}`);
+    });
+  }catch(error){
+    console.log(`error in setting up env\nerror: ${error.toString()}`);
+  }
+}
+
 const args = process.argv.slice(2);
 if (args.includes('-env')) {
   setUpEnv();
 } 
-if (args.includes('-d')) {
+else if (args.includes('-d')) {
   deploy();
 } 
+else if(args.includes('-upgrade')){
+  update();
+}
+
+else {
+  console.log(help);
+}
