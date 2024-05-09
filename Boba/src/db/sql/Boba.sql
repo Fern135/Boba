0fd763dfb18fdbb40c39fe3aa797574f5b2f41d1
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2024-05-09 01:09:01 -0400

    started to work on the sql so the Boba.db is always empty when initializing.

---
+++ Boba/src/db/sql/Boba.sql
@@ -0,0 +1,44 @@
+
+CREATE DATABASE Boba_Config;
+
+-- Create Software Table
+CREATE TABLE software (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    version VARCHAR(20) NOT NULL,
+    default_database VARCHAR(20) NOT NULL
+);
+
+-- Create Language Versions Table
+CREATE TABLE language_versions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    software_id INT,
+    language_name VARCHAR(20) NOT NULL,
+    version VARCHAR(20) NOT NULL,
+    is_installed BOOLEAN NOT NULL,
+    FOREIGN KEY (software_id) REFERENCES software(id)
+);
+
+-- Create Time Format Table
+CREATE TABLE time_format (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    software_id INT,
+    format VARCHAR(2) NOT NULL,
+    FOREIGN KEY (software_id) REFERENCES software(id)
+);
+
+-- Create Projects Path Table
+CREATE TABLE projects_path (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    software_id INT,
+    path VARCHAR(255) NOT NULL,
+    FOREIGN KEY (software_id) REFERENCES software(id)
+);
+
+-- Create Domains Table
+CREATE TABLE domains (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    software_id INT,
+    domain VARCHAR(255) NOT NULL,
+    route VARCHAR(255) NOT NULL,
+    FOREIGN KEY (software_id) REFERENCES software(id)
+);
