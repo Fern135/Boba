576ee88c5ef851fdcd110fca922a089350f7d5e3
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-05-13 17:48:46 -0400

    placeholder functions for SetUpApi, and Start for the api.go

--- Boba/src/api/api.go
+++ Boba/src/api/api.go
@@ -3,3 +3,11 @@ package api
 /*
 	api for handling the front end work.
 */
+
+func SetUpApi() {
+	println("Set up api before running. should run only once")
+}
+
+func Start() {
+	println("Start the server")
+}
