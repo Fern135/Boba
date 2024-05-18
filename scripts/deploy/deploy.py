7d2d1f1c90cae8b188f243ec9874c1aa310975f5
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-05-17 23:31:26 -0400

    deploy script begining
    todo: make process to download Boba landing page, and replacing the .exe
    todo: detect in the landing page what os the user has and show apropriate

--- scripts/deploy/deploy.py
+++ scripts/deploy/deploy.py
@@ -1,1 +1,19 @@
 # todo: run deploy script here. 
+
+import os
+
+def delete_file(filepath):
+  try:
+    os.remove(filepath)
+    return True
+  except FileNotFoundError:
+    return False
+
+
+API_SET_UP_DIR = "../../Boba/api/st.txt"
+deleted = delete_file(API_SET_UP_DIR)
+
+if deleted:
+    print("File deleted successfully!")
+else:
+    print("File not found.")
