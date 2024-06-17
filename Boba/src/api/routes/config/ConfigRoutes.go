c667e7c17cfdb7fb7e67eee5c836f8ab8edd9e34
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-06-17 12:37:56 -0400

    made the comments of what to do in the future

--- Boba/src/api/routes/config/ConfigRoutes.go
+++ Boba/src/api/routes/config/ConfigRoutes.go
@@ -5,7 +5,8 @@ import (
 	"net/http"
 )
 
-// todo: figure out how to make this work in terms of controller. CRUD (create, read, update and delete)
+// todo: figure out how to make this work in terms of controller.
+// todo: CRUD (create, read, update and delete)
 func ConfigRoutes() {
 	http.HandleFunc("/", config.HomeHandler)
 	http.HandleFunc("/about", config.AboutHandler)
--- Boba/src/api/routes/dns/DnsRoutes.go
+++ Boba/src/api/routes/dns/DnsRoutes.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 )
 
-// todo: figure out how to make this work in terms of controller. CRUD (create, read, update and delete)
+// todo: figure out how to make this work in terms of controller.
+//
+//	todo: CRUD (create, read, update and delete)
 func ConfigRoutes() {
 	http.HandleFunc("/", dns.HomeHandler)
 	http.HandleFunc("/about", dns.AboutHandler)
