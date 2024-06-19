4a4ebda9c2434f27549ade627e6639e4a4aa59ef
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-06-19 17:06:09 -0400

    "Rename handlers in PhpController, update PhpRoutes to use new handler names and add url list"

--- Boba/src/api/controllers/php/PhpController.go
+++ Boba/src/api/controllers/php/PhpController.go
@@ -7,10 +7,10 @@ import (
 
 // todo: figure out how to make this work in terms of controller.
 // CRUD (create, read, update and delete)
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the home page!")
 }
 
-func AboutHandler(w http.ResponseWriter, r *http.Request) {
+func CreateProject(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the about page!")
 }
--- Boba/src/api/routes/php/PhpRoutes.go
+++ Boba/src/api/routes/php/PhpRoutes.go
@@ -5,9 +5,20 @@ import (
 	"net/http"
 )
 
+var urls = []string{
+	"/",                // 0 [√]
+	"/create-project/", // 1 [√] -> connecting domain and db config,
+	"/read-project",    // 2 []
+	"/read-projects",   // 3 []
+	"/update-projects", // 4 [] updating db, php config files,
+}
+
 // todo: figure out how to make this work in terms of controller.
 // todo: CRUD (create, read, update and delete)
 func PhpRoutes() {
-	http.HandleFunc("/", php.HomeHandler)
-	http.HandleFunc("/about", php.AboutHandler)
+	go func() {
+		http.HandleFunc(urls[0], php.Home)
+		http.HandleFunc(urls[1], php.CreateProject)
+
+	}()
 }
